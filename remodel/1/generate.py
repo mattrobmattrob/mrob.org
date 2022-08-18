08fc4f964be28ecc1007c5105fa1679dbdc2e3d3
Matt Robinson <matt_rob@me.com>
2022-08-17 22:10:12 -0600

    Add individual pages

--- remodel/1/generate.py
+++ remodel/1/generate.py
@@ -3,38 +3,34 @@
 import glob
 import os
 
-html_header = """
+def html_header(relative_adjustment = ""):
+    return """
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <title>Remodel</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="{}css/style.css">
 </head>
 <body>
-"""
+""".format(relative_adjustment)
 
-html_footer = """
+def html_footer(relative_adjustment = ""):
+    return """
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
-    <script src="js/gallery.js"></script>
+    <script src="{}js/gallery.js"></script>
 
 </body>
 </html>
-"""
+""".format(relative_adjustment)
 
-sections_html = []
-for path in sorted(os.listdir("./photos"), reverse=True):
-    if path == ".DS_Store":
-        continue
-
-    section_title = path
-
+def section_html(section_title, image_paths):
     items_html = ["""
                             <div class="gallery-item item-3x4">
                                 <img class="thumb placeholder" src="{}" data-src="{}">
                             </div>
-""".format(image, image) for image in glob.glob("./photos/" + path + "/" + "*.png")]
+""".format(path, path) for path in image_paths]
 
     section_html = """
     <section>
@@ -52,10 +48,51 @@ for path in sorted(os.listdir("./photos"), reverse=True):
         </div>
     </section>
 """.format(section_title, "\n".join(items_html))
-    sections_html.append(section_html)
+    return section_html
 
-file_handler = open("index.html", "w")
-file_handler.write(html_header)
-file_handler.write("\n".join(sections_html))
-file_handler.write(html_footer)
-file_handler.close()
+def grouped_photos():
+    photo_groups = {}
+    for path in sorted(os.listdir("./photos"), reverse=True):
+        if path == ".DS_Store":
+            continue
+
+        photo_groups[path] = sorted([image for image in glob.glob("./photos/" + path + "/" + "*.png")])
+    return photo_groups
+
+def write_html(path, html_list):
+    file_handler = open(path, "w")
+    file_handler.write("\n".join(html_list))
+    file_handler.close()
+
+###############################################################################
+# Generate groups of photos
+###############################################################################
+
+photo_groups = grouped_photos()
+
+###############################################################################
+# Write main `index.html`
+###############################################################################
+
+write_html(
+    path = "index.html",
+    html_list = [html_header()] + [section_html(date, images) for date, images in photo_groups.items()] + [html_footer()]
+)
+
+###############################################################################
+# Write each section's separate `index.html`
+###############################################################################
+
+for date, images in photo_groups.items():
+    html_dir = os.path.dirname(images[0])
+
+    html = section_html(date, [os.path.basename(file_path) for file_path in images])
+
+    write_html(
+        path = "{}/index.html".format(html_dir),
+        html_list = [
+            html_header(relative_adjustment = "../../"),
+            html,
+            html_footer(relative_adjustment = "../../"),
+        ]
+    )
